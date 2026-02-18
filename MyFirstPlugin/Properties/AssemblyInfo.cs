3db17f3be60e6941f289d5113eb43a814d0c5f1f
vkusher <vadik1@gmail.com>
2026-02-18 15:56:27 +0200

    Create AssemblyInfo.cs file for MyFirstPlugin

---
+++ MyFirstPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyFirstPlugin")]
+[assembly: AssemblyDescription("Dataverse Plugin for Account table")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFirstPlugin")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("12345678-1234-1234-1234-123456789abc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
